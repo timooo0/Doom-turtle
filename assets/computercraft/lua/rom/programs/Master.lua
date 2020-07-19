61cc5d35f926294cae18c0408c10d2e882a030f0
Lukam0s <68496174+Lukam0s@users.noreply.github.com>
2020-07-19 10:48:46 +0200

    Update Master.lua
    Newer Version

--- assets/computercraft/lua/rom/programs/Master.lua
+++ assets/computercraft/lua/rom/programs/Master.lua
@@ -2,26 +2,10 @@ self = require("self")
 
 --Initialization
 
-if fs.exists("/Data.txt") == false then
---Create the Data.txt file and fill items
-	fs.copy("/rom/programs/dataTemplate.txt","/Data.txt")
-	write("x position: ")
-	local xPos = read()
-	self.store(1,xPos)
-	write("y position: ")
-	local yPos = read()
-	self.store(2,xPos)
-	write("z position: ")
-	local xPos = read()
-	self.store(3,xPos)
-	write("facing in direction:  ")
-	local xPos = read()
-	self.store(4,xPos)
 
-end
-
-
+self.checkShutdown()
 if self.get(14) == "Start" then
+	
 --Process from start
 	shell.run("StartTree")
 	
@@ -30,7 +14,7 @@ if self.get(14) == "Start" then
 	
 end	
 --Quarry
-
+self.checkShutdown()
 if self.get(14) == "QuarryInitialization" then
 		print("Init")
 		x = tonumber(self.get(1))
@@ -44,52 +28,86 @@ if self.get(14) == "QuarryInitialization" then
 		self.store(8, facing)
 
 	--The Quarry Size
-		self.store(9, 3)
-		self.store(10, 16)
-		self.store(11, 6)
+		self.store(9, 31)
+		--self.store(10, 16)
+		self.store(11, 60)
 
 	-- Initialization
-		self.store(12, 0)
+		--self.store(12, 0)
 		self.store(13, 0)
 
 
 	
---Restart Resistance
-	self.store(14, "Quarry")
-	--Initialization
-	self.store(15, 1)
-	self.store(16, 0)
+	--Restart Resistance
+		self.store(14, "Quarry")
+		--Initialization
+		self.store(15, 1)
+		self.store(16, "false")
+		
+		self.store(18, "false")
+		
+		self.store(20, 0)
+		self.store(21, 0)
+		self.store(22, 0)
+		self.store(23, 0)
 end
+self.checkShutdown()
 if self.get(14) == "Quarry" then
-	--Checks wether we have enough to build turtle
-	enoughItems = tonumber(self.get(16))
+
+	
+	
+	enoughItems = self.get(16)
+	enoughSand = self.get(18)
 	print(enoughItems)
-	if enoughItems == 0 then
+	if enoughItems == "false" then
 		print('hoi')
 		biggerQuarry = tonumber(self.get(15))
-		self.store(10, 16*biggerQuarry)
-		self.store(12, 16*(biggerQuarry-1))
+		--16 is for 1 chunk size
+		self.store(10, 1*biggerQuarry)
+		self.store(12, 1*(biggerQuarry-1))
+
 		shell.run("Quarry")
+	elseif enoughSand == "false" and enoughItems == "true" then
+		biggerQuarry = tonumber(self.get(15))
+		--16 is for 1 chunk size
+		self.store(10, 1*biggerQuarry)
+		self.store(12, 1*(biggerQuarry-1))
+		self.store(11, 12)
+		
+		shell.run("Quarry")
+	else
+		self.store(14, "startBase")
+	end
+end
+self.checkShutdown()
+if self.get(14) == "QuarryMore" then
+	--Restart Resistance
+		biggerQuarry = tonumber(self.get(15))
 		biggerQuarry = biggerQuarry+1
 		self.store(15, biggerQuarry)
-		--Check enough items
-		if false then
-			enoughItems = true
-			self.store(16, true)
-		end
-	end
-	
-	--Check wether collected everything necessary
-	
-	
-	--Restart Resistance
-	self.store(14, "TreeFarm")
-end
+	--Checks wether we have enough to build turtle
+		enoughItems = "true"
+		enoughSand = "true"
 
-if self.get(14) == "TreeFarm" then
+		if tonumber(self.get(20)) < 7 or tonumber(self.get(21)) < 1 or tonumber(self.get(23)) < 15 then
+			enoughItems = "false"
+		end
+		if tonumber(self.get(22)) < 6 then
+			enoughSand = "false"
+			self.store(18, enoughSand)
+		end
+		
+		self.store(16, enoughItems)
+		self.store(18, enoughSand)
+		self.store(14, "Quarry")
+		shell.run("Master")
+		
+end
+self.checkShutdown()
+if self.get(14) == "startBase" then
 --Tree Farm
-	print("TreeFarmStarting")
-	
+	self.store(17, 0)
+	shell.run("startBase")
 	
 end
 
