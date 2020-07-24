4123b05997cf77fa48c79499adc462474703630f
Timooo0 <t.vinke@student.tue.nl>
2020-07-24 20:54:06 +0200

    Implemented the new gps.buildTemplate in buildTreeFarm

--- assets/computercraft/lua/rom/programs/buildTreeFarm.lua
+++ assets/computercraft/lua/rom/programs/buildTreeFarm.lua
@@ -28,33 +28,8 @@ template =
 gps.faceLeft()
 gps.move(16)
 gps.moveUp(4)
-gps.moveChunk(0,0)
-gps.face(1)
 
-for i=1,16 do
-	for j=1,16 do
-		print(i,j)
-		if template[i][j] == 1 then
-			item.selectItem("minecraft:dirt")
-			turtle.placeDown()
-		end
-		if j ~= 16 then
-		gps.move()
-		end
-	end
-	if i~=16 and j~= 16 then
-		if i%2 == 1 then
-		gps.faceRight()
-		gps.move()
-		gps.faceRight()
-		else
-		gps.faceLeft()
-		gps.move()
-		gps.faceLeft()
-
-		end
-	end
-end
+gps.buildTemplate(template,"minecraft:dirt")
 
 gps.moveChunk(12,3)
 gps.face(2)
