79ad5694ffb0a4d82a356f37f34638a8114e94b4
Timooo0 <t.vinke@student.tue.nl>
2020-07-24 18:22:12 +0200

    Implemented fuel into tree farm

--- assets/computercraft/lua/rom/programs/newTreeFarm.lua
+++ assets/computercraft/lua/rom/programs/newTreeFarm.lua
@@ -60,6 +60,7 @@ end
 function cutTree()
 
 	local totalUp = 0
+	item.selectItem("minecraft:log")
 	gps.move()
 	file.store(27,1)
 	file.store(28,file.get(4))
@@ -74,16 +75,16 @@ function cutTree()
 	gps.faceLeft()
 	gps.move()
 	file.store(27,0)
-	
+
 	for i=0,totalUp-1 do
 		rotateCut()
 		gps.moveDown()
 	end
-	
+
 	gps.faceLeft()
 	gps.faceLeft()
 
-	
+
 	plant()
 end
 
@@ -112,6 +113,7 @@ end
 
 
 startup()
+print("startup complete")
 index = file.get(26)
 while true do
 	file.checkShutdown()
@@ -122,6 +124,37 @@ while true do
 	file.store(26, index)
 	if index == table.getn(route) then
 		index = 1
+
+		if item.selectItem("minecraft:log") then
+			gps.moveChunk(11,4)
+			turtle.dropDown(math.floor(turtle.getItemCount()/2))
+			gps.moveChunk(11,3)
+			gps.moveDown()
+			gps.face(2)
+
+			turtle.drop()
+
+			if turtle.getFuelLevel() < 500 then
+				gps.moveDown()
+				gps.moveChunk(11,4)
+
+				if turtle.suckUp() then
+					item.selectItem("minecraft:coal")
+					turtle.refuel()
+				end
+				gps.moveChunk(11,3)
+				gps.moveUp()
+
+			end
+
+			gps.moveUp()
+			gps.moveChunk(12,3)
+
+			while item.selectItem("minecraft:log") do
+				print("dropping")
+				turtle.dropDown()
+			end
+		end
 	end
 end
 end
--- assets/computercraft/lua/rom/programs/test.lua
+++ assets/computercraft/lua/rom/programs/test.lua
@@ -1,6 +1,8 @@
 self = require("self")
 buildTreeFarm = require("buildTreeFarm")
 startBase = require("startBase")
+newTreeFarm = require("newTreeFarm")
 os.loadAPI("/rom/apis/item.lua")
 
-startBase.Function()
+
+newTreeFarm.Function()
