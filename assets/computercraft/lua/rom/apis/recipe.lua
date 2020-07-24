0cb3e0eba3e973ad1e16cb66ac9e2c45a8d1eb29
Timooo0 <t.vinke@student.tue.nl>
2020-07-24 16:43:03 +0200

    Added recipe api

---
+++ assets/computercraft/lua/rom/apis/recipe.lua
@@ -0,0 +1,36 @@
+Computer = {}
+Computer["minecraft:stone,0"] = {1,2,3,5,7,9,11}
+Computer["minecraft:redstone"] = {6}
+Computer["minecraft:glass_pane"] = {10}
+
+Turtle = {}
+Turtle["minecraft:iron_ingot"] = {1,2,3,5,7,9,11}
+Turtle["computercraft:computer"] = {6}
+Turtle["minecraft:chest"] = {10}
+
+Modem = {}
+Modem["minecraft:stone,0"] = {1,2,3,5,7,9,10,11}
+Modem["minecraft:redstone"] = {6}
+
+ModemBlock = {}
+ModemBlock["computercraft:cable,1"] = {1}
+
+Furnace = {}
+Furnace["minecraft:cobblestone"] = {1,2,3,5,7,9,10,11}
+
+Chest = {}
+Chest["minecraft:planks"] = {1,2,3,5,7,9,10,11}
+
+GlassPane = {}
+GlassPane["minecraft:glass"] = {5,6,7,9,10,11}
+
+Stick = {}
+Stick["minecraft:planks"] = {1,5}
+
+DiamondPickaxe = {}
+DiamondPickaxe["minecraft:diamond"] = {1,2,3}
+DiamondPickaxe["minecraft:stick"] = {6,10}
+
+MiningTurtle = {}
+MiningTurtle["minecraft:diamond_pickaxe"] = {3}
+MiningTurtle["computercraft:turtle_expanded"] = {2}
