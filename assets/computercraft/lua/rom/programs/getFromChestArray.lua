bf34a59b7c7563d8d98bd692e61cd7ca28a1af89
Timooo0 <t.vinke@student.tue.nl>
2020-07-29 21:53:34 +0200

    Works with the new chestMap

--- assets/computercraft/lua/rom/programs/getFromChestArray.lua
+++ assets/computercraft/lua/rom/programs/getFromChestArray.lua
@@ -15,13 +15,14 @@ boodschapjes = {}
 boodschapjes["minecraft:chest"] = 2
 boodschapjes["minecraft:stained_glass"] = 12
 boodschapjesState = true
+
 for i=1,8 do
   for j=1,table.getn(route) do
     if boodschapjesState then
 
       if (file.get(1)%16)%2 == 0 and (file.get(3)%16)%2==1 then
-        local chestItemPlus = chestMap[file.get(1)%16+1][file.get(3)%16+2]
-        local chestItemMin = chestMap[file.get(1)%16+1][file.get(3)%16]
+        local chestItemPlus = chestMap[file.get(1)%16+1][file.get(3)%16+2][1]
+        local chestItemMin = chestMap[file.get(1)%16+1][file.get(3)%16][1]
         for key, value in pairs(boodschapjes) do
           if key == chestItemPlus then
             gps.face(2)
@@ -34,7 +35,7 @@ for i=1,8 do
           end
         end
       else
-        local chestItem = chestMap[file.get(1)%16+1][file.get(3)%16+1]
+        local chestItem = chestMap[file.get(1)%16+1][file.get(3)%16+1][1]
         for key, value in pairs(boodschapjes) do
           if chestItem == key then
             item.getFromChest(key,value)
