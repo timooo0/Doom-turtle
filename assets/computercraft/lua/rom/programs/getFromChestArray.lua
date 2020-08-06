05ae1100ba73722bba8483558da56f4abc1e17d3
Timooo0 <t.vinke@student.tue.nl>
2020-08-07 00:30:26 +0200

    Can now look in the entire column

--- assets/computercraft/lua/rom/programs/getFromChestArray.lua
+++ assets/computercraft/lua/rom/programs/getFromChestArray.lua
@@ -4,33 +4,75 @@ os.loadAPI("/rom/apisFiles/item.lua")
 os.loadAPI("/rom/apisFiles/recipe.lua")
 
 local route = {
-  1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-  2,2,
   3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,
   1,
+  2,2,
+  1,1,1,1,1,1,1,1,1,1,1,1,1,1,
   2,2
 }
+
+baseY = file.get(2)
 chestMap = file.getTable("chestMap.txt")
 boodschapjes = {}
 boodschapjes["minecraft:chest"] = 2
-boodschapjes["minecraft:stained_glass"] = 12
+boodschapjes["minecraft:concrete_powder"] = 12
 boodschapjesState = true
 
-for i=1,8 do
+print("shopping list:")
+for k,v in pairs(boodschapjes) do
+  print(k .. " " .. v)
+end
+
+function getFromChestColumn(name,amount)
+  print("looking for " .. name)
+  moveBack = false
+  if select(2,turtle.inspectUp()).name == "minecraft:chest" then
+    moveBack = true
+    amount = amount - item.getFromChest(name,amount)
+
+    if amount ~= 0 then
+      gps.move()
+      gps.faceAround()
+      gps.moveUp(3)
+    end
+  end
+
+
+  while select(2,turtle.inspect()).name == "minecraft:chest" and amount ~= 0 do
+    amount = amount - item.getFromChest(name,amount)
+    if amount ~= 0 then
+      gps.moveUp(2)
+    end
+  end
+
+  while file.get(2) ~= baseY do
+    gps.moveDown()
+  end
+  if moveBack then
+    gps.move()
+  end
+end
+
+
+for i=1,4 do
   for j=1,table.getn(route) do
     if boodschapjesState then
 
       if (file.get(1)%16)%2 == 0 and (file.get(3)%16)%2==1 then
-        local chestItemPlus = chestMap[file.get(1)%16+1][file.get(3)%16+2][1]
+        if file.get(3)%16+2 <= 16 then
+          local chestItemPlus = chestMap[file.get(1)%16+1][file.get(3)%16+2][1]
+        end
         local chestItemMin = chestMap[file.get(1)%16+1][file.get(3)%16][1]
         for key, value in pairs(boodschapjes) do
           if key == chestItemPlus then
             gps.face(2)
-            item.getFromChest(key,value)
+            getFromChestColumn(key,value)
+            print("got " .. key)
             boodschapjes[key] = nil
           elseif key == chestItemMin then
             gps.face(0)
-            item.getFromChest(key,value)
+            getFromChestColumn(key,value)
+            print("got " .. key)
             boodschapjes[key] = nil
           end
         end
@@ -38,7 +80,8 @@ for i=1,8 do
         local chestItem = chestMap[file.get(1)%16+1][file.get(3)%16+1][1]
         for key, value in pairs(boodschapjes) do
           if chestItem == key then
-            item.getFromChest(key,value)
+            getFromChestColumn(key,value)
+            print("got " .. key)
             boodschapjes[key] = nil
           end
         end
