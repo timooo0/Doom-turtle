6c1db8ef6485a7244400073a0c519d2893dc3484
Timooo0 <t.vinke@student.tue.nl>
2020-08-11 15:28:50 +0200

    deleting test.lua

--- assets/computercraft/lua/rom/programs/test.lua
+++
@@ -1,9 +0,0 @@
-self = require("self")
-buildTreeFarm = require("buildTreeFarm")
-startBase = require("startBase")
-newTreeFarm = require("newTreeFarm")
--- chunkMap = require("chunkMap")
--- chestArray = require("chestArray")
-os.loadAPI("/rom/apisFiles/file.lua")
-
-
