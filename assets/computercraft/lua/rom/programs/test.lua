4c8e5c02e898ad1fa741bf5c768d2f27fa5909bd
Timooo0 <t.vinke@student.tue.nl>
2020-08-11 15:21:57 +0200

    Fix gitigore

--- assets/computercraft/lua/rom/programs/test.lua
+++ assets/computercraft/lua/rom/programs/test.lua
@@ -2,15 +2,37 @@ self = require("self")
 buildTreeFarm = require("buildTreeFarm")
 startBase = require("startBase")
 newTreeFarm = require("newTreeFarm")
-chunkMap = require("chunkMap")
+-- chunkMap = require("chunkMap")
 -- chestArray = require("chestArray")
-os.loadAPI("/rom/apis/gps.lua")
+os.loadAPI("/rom/apisFiles/file.lua")
 
-for i=1,4 do
-  for j=1,3 do
-    print(i,j)
-    if j == 2 then
-      break
+chestMap = {}
+for i=1,16 do
+  chestMap[i] = {}     -- create a new row
+  for j=1,16 do
+    chestMap[i][j] = {}
+    for k=1,2 do
+      chestMap[i][j][k] = 0
     end
   end
 end
+
+for i=1,16 do
+  for j=1,16 do
+    for k=1,2 do
+    str = tostring(i)..","..tostring(j)..","..tostring(k)
+    chestMap[i][j][k] = str
+    end
+  end
+end
+
+for i=1,2 do
+  for j=1,2 do
+    for k=1,2 do
+    print(chestMap[i][j][k])
+    end
+  end
+end
+-- file.storeTable(mt,"test.txt")
+-- print(table.getn(chestMap))
+-- print(table.getn(chestMap[1]))
